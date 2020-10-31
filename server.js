eaaaaa7bb18559cb591b76cb9bcbdd9bb1e28e15
Allister Rampenthal <allisterrampenthal@gmail.com>
2020-10-31 18:28:51 -0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+
+const app = express();
+
+const PORT = process.env.PORT || 80;
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+const controller = require('./app/controllers/burgerController.js');
+
+app.use(controller);
+
+app.listen(PORT, function(){
+    console.log('Server listening on: http://localhost:80');
+});
