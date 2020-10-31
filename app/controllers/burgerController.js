3ec127aca05606e40026b2018bd963ca7dff1725
Allister Rampenthal <allisterrampenthal@gmail.com>
2020-10-31 18:28:33 -0400

    Create burgerController.js

---
+++ app/controllers/burgerController.js
@@ -0,0 +1,31 @@
+const express = require('express');
+
+const router = express.Router();
+
+const burger = require('../models/burger');
+
+router.get('/', function(req, res){
+    
+});
+
+router.get('/api/burgers/orders', function(req, res){
+
+});
+
+router.put('/api/burgers/orders/:id', function(req, res){
+
+});
+
+router.post('/api/burgers/orders', function(req, res){
+
+});
+
+router.delete('/api/burgers/orders/:id', function(req, res){
+
+});
+
+
+
+
+
+module.exports = router;
