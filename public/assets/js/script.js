1b726fb0a24a748870bba75295d161c67e2c6a3b
Allister Rampenthal <allisterrampenthal@gmail.com>
2020-11-04 13:38:55 -0500

    Update script.js

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,4 +1,18 @@
-$('#form').on('submit', function(event){
+$('#form').on('submit', function (event) {
     event.preventDefault();
     alert("hi");
+});
+
+$(".eatBtn").on("click", function (event) {
+    event.preventDefault();
+    const id = $(this).data("id");
+
+    $.ajax({
+        method: "PUT",
+        url: "/api/burgers/orders/"+id,
+        data: "isEaten = true"
+    }).then(function(response){
+        console.log(response);
+        location.reload();
+    });
 });
