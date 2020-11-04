e6f5014d9fea30662cba528372c456e16afbeeb4
Allister Rampenthal <allisterrampenthal@gmail.com>
2020-11-03 21:16:28 -0500

    Update orm.js

--- app/config/orm.js
+++ app/config/orm.js
@@ -1,5 +1,15 @@
 const connection = require('./connection');
 
+const orm = {
+    selectAll: function(table, cb) {
+        let query = "SELECT * FROM " + table;
+        connection.query(query, function(err, result){
+            if(err) throw err;
 
+            cb(result);
+        });
+    },
+    
+};
 
 module.exports = orm;
