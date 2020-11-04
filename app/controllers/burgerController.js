217284c590bb1ea28b6fb4d6fe8a0443140d46e8
Allister Rampenthal <allisterrampenthal@gmail.com>
2020-11-03 21:54:21 -0500

    Update burgerController.js

--- app/controllers/burgerController.js
+++ app/controllers/burgerController.js
@@ -7,25 +7,22 @@ const burger = require('../models/burger');
 router.get('/', function(req, res){
     burger.selectAll(function(data){
         console.log(data);
-    });
-});
-
-router.get('/api/burgers/orders', function(req, res){
-    burger.selectAll(function(data){
-        console.log(data);
+        res.render("index", { burgers: data });
     });
 });
 
 router.put('/api/burgers/orders/:id', function(req, res){
-
+    console.log(req.body);
 });
 
 router.post('/api/burgers/orders', function(req, res){
-
+    console.log(req.body);
 });
 
 router.delete('/api/burgers/orders/:id', function(req, res){
-
+    burger.delete(req.params.id, function(data){
+        console.log(data);
+    });
 });
 
 
