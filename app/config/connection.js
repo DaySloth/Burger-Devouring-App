b4a8e32b73da95a34be6a01869cf9a18e352f022
Allister Rampenthal <allisterrampenthal@gmail.com>
2020-11-03 22:05:08 -0500

    added heroku

--- app/config/connection.js
+++ app/config/connection.js
@@ -2,17 +2,21 @@ const mysql = require('mysql');
 
 require('dotenv').config();
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: 'Batmanisc00l',
-    database: 'burger_db',
-    pool: {
-        max: 7,
-        min: 0,
-        idle: 10000
-    }
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    const connection = mysql.createConnection({
+        host: 'localhost',
+        port: 3306,
+        user: 'root',
+        password: 'Batmanisc00l',
+        database: 'burger_db',
+        pool: {
+            max: 7,
+            min: 0,
+            idle: 10000
+        }
+    });
+};
 
 module.exports = connection;
