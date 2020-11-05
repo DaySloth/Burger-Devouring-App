cdb56b522797d31315102c42e03b32bf708d730a
Allister Rampenthal <allisterrampenthal@gmail.com>
2020-11-04 19:13:26 -0500

    Update script.js

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,18 +1,36 @@
 $('#form').on('submit', function (event) {
-    event.preventDefault();
-    alert("hi");
+    $.ajax("/api/burgers/orders", {
+        type: "POST",
+        data: { burger_name: $('#burger').val() }
+    }).then(function (response) {
+        //console.log(response);
+        location.reload();
+    })
 });
 
 $(".eatBtn").on("click", function (event) {
-    event.preventDefault();
+    //event.preventDefault();
     const id = $(this).data("id");
 
     $.ajax({
         method: "PUT",
-        url: "/api/burgers/orders/"+id,
+        url: "/api/burgers/orders/" + id,
         data: { isEaten: true }
-    }).then(function(response){
-        console.log(response);
+    }).then(function (response) {
+        //console.log(response);
+        location.reload();
+    });
+});
+
+$(".throwAwayBtn").on("click", function (event) {
+    //event.preventDefault();
+    const id = $(this).data("id");
+
+    $.ajax({
+        method: "DELETE",
+        url: "/api/burgers/orders/" + id
+    }).then(function (response) {
+        //console.log(response);
         location.reload();
     });
 });
